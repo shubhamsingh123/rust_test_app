9ddf20667d4c2879a9905dd555b34156aaa59714
shubhamsingh123 <shubham16cse06@gmail.com>
2025-04-15 02:35:58 +0530

    Check the custom input in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::io;
+
 fn main(){
-   let x = 100;
+
+    println!("Enter a number : ");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+//    let x = 100;
+
+let x: u32 = input.trim().parse().expect("Please enter a valid number");
    let is_even = ans(x);
 
    if is_even{
