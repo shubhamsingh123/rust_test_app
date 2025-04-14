4daeceb2bcdbc7241a01c3788d19eba23996827f
shubhamsingh123 <shubham16cse06@gmail.com>
2025-04-15 02:14:49 +0530

    Checking the boolean data types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main(){
-    let ans = sum(1, 2);
-    println!("{}" , ans);
+    // let ans = sum(1, 2);
+    let is_even = bool(10);
+    println!("{}" , is_even);
 }
 
-fn sum(a:u32 , b:u32)->u32{
-  return a+b
+fn bool(n:u32)->bool{
+  return n%2==0;
 }
