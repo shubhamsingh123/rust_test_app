d1f67ad8dda4f7e2c7433a4c1599b1c77676a158
shubhamsingh123 <shubham16cse06@gmail.com>
2025-04-15 02:29:20 +0530

    check the conditional data type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 fn main(){
-    // let str = String::from("Hello World");
-    let v = vec![1 , 2  , 3];
-    println!("{:?}" , v);
+   let x = 100;
+   let is_even = ans(x);
+
+   if is_even{
+    println!("{}" , "Congratulation it is an even number")
+   }else{
+    println!("{}" , "It is an odd number")
+   }
+}
+
+fn ans(x:u32)->bool{
+    return x%2==0;
 }
