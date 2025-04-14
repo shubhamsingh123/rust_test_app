f4ae9c0a2b978674c0bc2c8be9cd01dfa6781df6
shubhamsingh123 <shubham16cse06@gmail.com>
2025-04-15 02:39:17 +0530

    loop data type in rust

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-use std::io;
-
 fn main(){
-
-    println!("Enter a number : ");
-
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
-
-//    let x = 100;
-
-let x: u32 = input.trim().parse().expect("Please enter a valid number");
-   let is_even = ans(x);
-
-   if is_even{
-    println!("{}" , "Congratulation it is an even number")
-   }else{
-    println!("{}" , "It is an odd number")
-   }
-}
-
-fn ans(x:u32)->bool{
-    return x%2==0;
+    for i in 0..100{
+        println!("{}" , i);
+    }
 }
